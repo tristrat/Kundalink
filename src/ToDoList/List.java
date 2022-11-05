f97746629bd821162ac866e93efd24121f439d9e
MatD666 <dupriez.matteo@gmail.com>
2022-11-05 18:50:27 +0100

    some complement

--- src/ToDoList/List.java
+++ src/ToDoList/List.java
@@ -1,2 +1,59 @@
 package ToDoList;public class List {
+    String title;
+    String contant;
+    int id;
+    boolean completed;
+    public List() {
+        new List("missing","missing",-1);
+    }
+
+    public List(String title, String contant) {
+        new List(title,contant,-1);
+    }
+
+    public List(String title, String contant, int id) {
+        this.title = title;
+        this.contant = contant;
+        this.id = id;
+        completed=false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContant() {
+        return contant;
+    }
+
+    public void setContant(String contant) {
+        this.contant = contant;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean finished(){
+        if (completed){
+            return false;
+        }
+        completed=true;
+        return true;
+    }
+    public boolean unfinished(){
+        if (completed){
+            completed=false;
+            return true;
+        }
+        return false;
+    }
 }
