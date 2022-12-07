249c8a632cc6c4bdcd6b2862979705755ec730b9
Carla802 <113369817+Carla802@users.noreply.github.com>
2022-12-07 18:40:26 +0100

    Delete src directory

--- src/calendrier/semaine.java
+++
@@ -1,9 +0,0 @@
-package calendrier;
-
-public class semaine extends Mois{
-    int numSemaine;
-    public semaine(int Anne , String mois ,int numSemaine){
-        super(Anne,mois);
-        this.numSemaine=numSemaine;
-    }
-}
